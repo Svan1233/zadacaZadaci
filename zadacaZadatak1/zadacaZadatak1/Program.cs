133cb3c4b40e316f66aef7fa684cafc6a0aacb73
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2020-01-16 15:22:41 +0100

    Add files via upload

---
+++ zadacaZadatak1/zadacaZadatak1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacaZadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 3;
+            int x;
+            float y;
+
+            x = a / b;
+            y = a % b;
+
+            Console.WriteLine(a+" podjeljeno sa "+b+" je "+x+" a ostatak je "+y);
+            Console.ReadKey();
+        }
+    }
+}
